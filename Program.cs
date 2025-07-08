a5a8f07e4781447cd2046590d40cc968d4114d46
haquocdat543 <wwwdatha543@gmail.com>
2025-07-08 09:38:30 +0700

    (code): format

--- Program.cs
+++ Program.cs
@@ -10,28 +10,37 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	"Freezing",
+	"Bracing",
+	"Chilly",
+	"Cool",
+	"Mild",
+	"Warm",
+	"Balmy",
+	"Hot",
+	"Sweltering",
+	"Scorching",
 };
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+  var forecast = Enumerable.Range(1, 5).Select(index =>
+	  new WeatherForecast
+	  (
+		  DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+		  Random.Shared.Next(-20, 55),
+		  summaries[Random.Shared.Next(summaries.Length)]
+	  ))
+	  .ToArray();
+  return forecast;
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
@@ -40,5 +49,5 @@ app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
