d5817e0fd4a983dc0f7aefccfb97dd1f41b1922b
haquocdat543 <wwwdatha543@gmail.com>
2025-07-09 21:10:34 +0700

    (module/user): add dto

---
+++ Module/User/DTOs/CreateUserDTO.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Module.User.DTOs;
+
+public class CreateUserDTO
+{
+  [Required, MinLength(3), MaxLength(20)]
+  public string Name { get; set; } = string.Empty;
+
+  [Required, EmailAddress]
+  public string Email { get; set; } = string.Empty;
+
+  [Required, MinLength(8), MaxLength(20)]
+  public string Password { get; set; } = string.Empty;
+}
+
